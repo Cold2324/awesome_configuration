cded7898c8c5e8fbdf50383e4e2d8ebfb3a3ca60
Gonzalo Cabada <cabadagonzalo17@gmail.com>
2022-12-10 16:50:26 -0500

    add '|'

--- rc.lua
+++ rc.lua
@@ -276,6 +276,11 @@ awful.screen.connect_for_each_screen(function(s)
             widget = wibox.container.rotate
           },
           {
+            textbox(" | "),
+            direction = "east",
+            widget = wibox.container.rotate
+          },
+          {
             mytextclock,
             direction = "east",
             widget = wibox.container.rotate
