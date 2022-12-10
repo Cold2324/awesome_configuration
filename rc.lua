bea54a5fa5c60be540fd7c931c2347e70f406dcc
Gonzalo Cabada <cabadagonzalo17@gmail.com>
2022-12-10 16:46:32 -0500

    aditional config

--- rc.lua
+++ rc.lua
@@ -68,6 +68,7 @@ modkey = "Mod4"
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
     awful.layout.suit.tile,
+    awful.layout.suit.floating,
 }
 -- }}}
 
@@ -526,7 +527,7 @@ root.keys(globalkeys)
 -- Rules to apply to new clients (through the "manage" signal).
 awful.rules.rules = {
     -- All clients will match this rule.
-    { rule = { },
+    { rule = {},
       properties = { border_width = beautiful.border_width,
                      border_color = beautiful.border_normal,
                      focus = awful.client.focus.filter,
