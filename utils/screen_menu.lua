ed9d5718253b5118737050ee12042cacb2d0191b
Gonzalo Cabada <cabadagonzalo17@gmail.com>
2022-12-19 23:19:19 -0500

    change keyboard layout function added to screen_menu

--- utils/screen_menu.lua
+++ utils/screen_menu.lua
@@ -4,16 +4,18 @@ local hotkeys_popup = require("awful.hotkeys_popup")
 awesome_menu = {
   { "Hotkeys", function() hotkeys_popup.show_help(nil, awful.screen.focused()) end },
   { "Manual", terminal .. " -e man awesome" },
-  { "Edit config", editor_cmd .. " " .. awesome.conffile }
+  { "Edit config", editor_cmd .. " " .. awesome.conffile },
+  { "Latam keyboard", function() os.execute('setxkbmap latam') end },
+  { "Us keyboard", function() os.execute('setxkbmap us') end }
 }
 
 screen_menu = awful.menu({
- items = {
-   { "  Awesome", awesome_menu },
-   { "  Open terminal", terminal },
-   { "  Browser", browser},
-   { "  Files", files_manager},
- }
+  items = {
+    { "  Awesome", awesome_menu },
+    { "  Open terminal", terminal },
+    { "  Browser", browser },
+    { "  Files", files_manager },
+  }
 })
 
 return screen_menu
